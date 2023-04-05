8ad859c27d18ae00046e67dfdd5b03c1b0737f53
brendon-burd <62077193+brendon-burd@users.noreply.github.com>
2023-04-05 15:52:16 -0500

    upload unit test changes

--- CodeTratherTeacher/CodeTratherTeacher/Form1.Designer.cs
+++ CodeTratherTeacher/CodeTratherTeacher/Form1.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.createExecButton = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // createExecButton
+            // 
+            this.createExecButton.Location = new System.Drawing.Point(112, 200);
+            this.createExecButton.Name = "createExecButton";
+            this.createExecButton.Size = new System.Drawing.Size(115, 73);
+            this.createExecButton.TabIndex = 0;
+            this.createExecButton.Text = "Create Executive Overview ";
+            this.createExecButton.UseVisualStyleBackColor = true;
+            this.createExecButton.Click += new System.EventHandler(this.createExec);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(268, 200);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 73);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Grade with Unit Test";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.gradeUnitTest);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.createExecButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button createExecButton;
+        private Button button3;
     }
 }
--- CodeTratherTeacher/CodeTratherTeacher/Form1.cs
+++ CodeTratherTeacher/CodeTratherTeacher/Form1.cs
@@ -1,3 +1,6 @@
+using System.Windows.Forms;
+using System.IO;
+
 namespace CodeTratherTeacher
 {
     public partial class Form1 : Form
@@ -5,6 +8,65 @@ namespace CodeTratherTeacher
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public static string filePath = Environment.GetEnvironmentVariable("USERPROFILE") + @"\" + "Downloads/TratherLogs/";
+        public static string unitTestFilePath = filePath + "unitTest.py";
+        public static string inputFilePath = "";
+
+        private OpenFileDialog openFileDialog;
+
+        private void getUnitTest()
+        {
+            // Set up the open file dialog
+            openFileDialog = new OpenFileDialog();
+            //wait for file to be selected
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //check if derectory or single file
+                if (openFileDialog.FileName.Contains(".py"))
+                {
+                    System.IO.File.WriteAllText(unitTestFilePath, System.IO.File.ReadAllText(openFileDialog.FileName));
+                }
+                else
+                {
+                    foreach (string file in Directory.EnumerateFiles(openFileDialog.FileName, "*.py"))
+                    {
+                        string contents = File.ReadAllText(file);
+                        MessageBox.Show(contents);
+                    }
+                }
+            }
+        }
+
+        private string runUnitTest() {
+
+            //get file
+            System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
+            //pProcess.StartInfo.CreateNoWindow = true;
+            pProcess.StartInfo.UseShellExecute = false;
+            pProcess.StartInfo.FileName = "cmd.exe";
+            pProcess.StartInfo.Arguments = "/C python " + unitTestFilePath + " " + inputFilePath;
+            // code either compiles or it doesn't
+            pProcess.StartInfo.RedirectStandardOutput = true;
+            pProcess.StartInfo.RedirectStandardError = true;
+            // start the command prompt
+            pProcess.Start();
+            string output = pProcess.StandardOutput.ReadToEnd();
+            string error = pProcess.StandardError.ReadToEnd();
+            pProcess.WaitForExit();
+            inputFilePath = "";
+            return output + error;
+        }
+
+        private void createExec(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void gradeUnitTest(object sender, EventArgs e)
+        {
+            getUnitTest();
         }
     }
 }
